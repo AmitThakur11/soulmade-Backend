589379df47ad963affa66bb915df2d798c10f5dd
amit thakur <amithakur081@gmail.com>
2021-09-15 02:58:21 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        name :"amit",
+        cast : "thakur"
+    },
+    {
+        name :"siddharth",
+        cast :"shukla"
+    }
+]
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const data = require("./data")
+const app = express();
+
+
+let port = process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    res.send("welcome on server")
+})
+app.get("/product",(req,res)=>{
+    res.json(data)
+})
+
+app.listen(port , ()=>`server listening on ${port}`)

e1c4ff3dc16381e3d8d0a377b3d221df58c76777
amit thakur <amithakur081@gmail.com>
2021-09-15 03:09:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ let port = process.env.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("welcome on server")
 })
-app.get("/product",(req,res)=>{
-    res.json({data})
-})
+
 
 app.listen(port , ()=>`server listening on ${port}`)
